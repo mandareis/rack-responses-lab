71e932422c1e6aceb401b8b92e6d5b284b853f07
mandareis <amanda@depaula.xyz>
2020-10-24 15:20:06 -0700

    Done.

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+require "pry"
+
+class Application
+  def call(env)
+    binding.pry
+    resp = Rack::Response.new
+
+    current_time = Time.now
+    if current_time.hour < 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+    resp.finish
+  end
+end
